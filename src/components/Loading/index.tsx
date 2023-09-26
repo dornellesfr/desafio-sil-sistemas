58900b15761be863a4e67b81a06a85661666d9ac
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-09-26 10:06:40 -0300

    feat: component loading is already done

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,12 @@
+import spinner from '../../assets/loading.svg';
+import StyledLoading from './style';
+
+function Loading() {
+  return (
+    <StyledLoading>
+      <img src={spinner} alt="spinner" />
+    </StyledLoading>
+  )
+}
+
+export default Loading;
---
+++ src/components/Loading/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledLoading = styled.div`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+
+  & img {
+    width: 50px;
+  }
+`;
+
+export default StyledLoading;
