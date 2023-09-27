ae22591fd5bda913465fb1375ce464f240cffc64
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-09-27 08:19:17 -0300

    feat: implementing cards but without style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-// import { useState } from "react";
+import { useEffect, useState } from "react";
+import getDataApi from "./utils/getApiData";
 
 import Header from "./components/Header";
 import Card from "./components/Card";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import StarRateIcon from '@mui/icons-material/StarRate';
 import dataCovid from "./helpers/dataCovidCases";
 
 function App() {
+  const [covidData, setCovidData] = useState([]);
+
+  // pegar dados da api de covid :ICovidCases
+  useEffect(() => {
+    getDataApi('...')
+  }, [])
+  // pegar dados da api de posts :IPosts
+
   return (
     <>
       <Header />
-      <Card props={}/>
+      <Card
+        icon={<VisibilityIcon />}
+        textTop="Confirmed cases"
+        textBottom="Learn how to prevent infections ➝"
+        color="fff"
+        content={dataCovid}
+      />
+      <Card
+        icon={<FormatListBulletedIcon />}
+        textTop="Top posts"
+        textBottom="Do you want more visits? Contact us!"
+        color="fff"
+        content={dataCovid}
+      />
+      <Card
+        icon={<StarRateIcon />}
+        textTop="Top posts"
+        textBottom="Do you want more visits? Contact us!"
+        color="#000032"
+        content={dataCovid}
+      />
     </>
   )
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,38 @@
+import { ChangeEventHandler, useState } from 'react';
 import ICard from '../../interfaces/ICard';
+import Loading from '../Loading';
 
-function Card(props: ICard[]) {
+function Card(props: ICard) {
+  const [mainCardText, setMainCardText] = useState(props.content[0].main);
+  // const [isLoading, setIsLoading] = useState(true);
+
+  if (!props.content[0].select) {
+    return (
+      <section>
+        <span>{props.icon}</span>
+        <p>{mainCardText}</p>
+        <p>{props.textTop}</p>
+        <p>{props.textBottom}</p>
+      </section>
+    );
+  }
+
+  function changeSelectTextCard(event: React.ChangeEvent<HTMLSelectElement>) {
+    setMainCardText(props.content[parseInt(event.target.value)].main)
+  }
+
   return (
-    <div>index</div>
+    <section>
+      <span>{props.icon}</span>
+      <select onChange={ changeSelectTextCard }>
+        { props.content.map(({ id, select }) => (
+          <option key={id} value={id}>{select}</option>
+        )) }
+      </select>
+      <p>{mainCardText}</p>
+      <p>{props.textTop}</p>
+      <p>{props.textBottom}</p>
+    </section>
   )
 }
 
