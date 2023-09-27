acd882c08fa03e0b9c7c77ebc3bd0117a332e4cf
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-09-27 08:17:44 -0300

    chore: just to mock data in js

--- src/interfaces/ICovidCases.ts
+++
@@ -1,8 +0,0 @@
-interface ICovidCases {
-  id: number;
-  uf: string;
-  name: string;
-  confirmed_cases: number;
-}
-
-export default ICovidCases;
---
+++ src/interfaces/IDataCard.ts
@@ -0,0 +1,7 @@
+interface IDataCard {
+  id: number;
+  select?: string;
+  main: number;
+}
+
+export default IDataCard;
