21c2e65e29bd91c8e82a92a84c97db5bb8054ed0
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-09-27 09:01:55 -0300

    WIP: Trustpilot card without style

--- src/components/TrustPilotCard/index.tsx
+++ src/components/TrustPilotCard/index.tsx
@@ -1,7 +1,13 @@
+import StarRateIcon from '@mui/icons-material/StarRate';
+
 function TrustPilotCard() {
   return (
     <section>
-
+      <StarRateIcon />
+      <p>Trustpilot</p>
+      <p>Show us your love bu leaving a <strong>positive</strong> review
+      on trust pilot and recieve the extension of a 50 additional products</p>
+      <p>Write a review on Trustpilot ➝</p>
     </section>
   );
 }
