7ca86391028e166cd536dbd1e5d8e1512d050803
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-09-28 21:56:58 -0300

    interface: add covid data's

---
+++ src/helpers/covidCases.ts
@@ -0,0 +1,34 @@
+import ICovidCasesStates from '../interfaces/ICovidCasesStates';
+
+const statesCases: ICovidCasesStates =
+  {
+    AC: 999,
+    AL: 845,
+    AP: 333,
+    AM: 242,
+    BA: 4012,
+    CE: 897,
+    DF: 3232,
+    ES: 581,
+    GO: 442,
+    MA: 991,
+    MT: 182,
+    MS: 137,
+    MG: 5431,
+    PA: 212,
+    PB: 432,
+    PR: 1427,
+    PE: 544,
+    PI: 789,
+    RJ: 9321,
+    RN: 1001,
+    RS: 4391,
+    RO: 328,
+    RR: 191,
+    SC: 3971,
+    SP: 15993,
+    SE: 987,
+    TO: 693
+  };
+
+export default statesCases;
---
+++ src/interfaces/ICovidCasesStates.ts
@@ -0,0 +1,31 @@
+interface ICovidCasesStates {
+  AC: number;
+  AL: number;
+  AP: number;
+  AM: number;
+  BA: number;
+  CE: number;
+  DF: number;
+  ES: number;
+  GO: number;
+  MA: number;
+  MT: number;
+  MS: number;
+  MG: number;
+  PA: number;
+  PB: number;
+  PR: number;
+  PE: number;
+  PI: number;
+  RJ: number;
+  RN: number;
+  RS: number;
+  RO: number;
+  RR: number;
+  SC: number;
+  SP: number;
+  SE: number;
+  TO: number;
+}
+
+export default ICovidCasesStates;
