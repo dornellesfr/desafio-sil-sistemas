a72052338c60e9a0e46b539d104685fc4716676a
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-09-28 21:58:20 -0300

    WIP: remaking logic of card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,25 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import ICard from '../../interfaces/ICard';
 // import Loading from '../Loading';
 import StyledCard from './style';
+import getDataApi from '../../utils/getApiData';
 
 function Card(props: ICard) {
-  const [mainCardText, setMainCardText] = useState(props.content[0].main);
+  const [cardContent, setCardContent] = useState(-1);
+  const [selectItem, setSelectItem] = useState(props.options[0].name)
+
+  useEffect(() => {
+    const dataCard = async () => {
+      const result: number = await getDataApi(`api/covidstatesbr/${selectItem}`);
+      setCardContent(result);
+    }
+
+    dataCard();
+  }, [selectItem]);
 
   function changeSelectTextCard(event: React.ChangeEvent<HTMLSelectElement>) {
-    setMainCardText(props.content[parseInt(event.target.value)].main)
+    setSelectItem(event.target.value)
   }
 
   return (
     <StyledCard>
       <h2><i>{props.icon}</i>{props.textTop}</h2>
       <select onChange={ changeSelectTextCard }>
-        { props.content.map(({ id, select }) => (
-          <option key={id} value={id}>{select}</option>
+        { props.options.map(({ id, name }) => (
+          <option key={id} value={name}>{selectItem}</option>
           )) }
       </select>
-      <p className='primary-info'>{mainCardText}</p>
+      <p className='primary-info'>{cardContent}</p>
       <a className='link-info' href="#">{props.textBottom}</a>
     </StyledCard>
   )
