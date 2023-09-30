7e2a8e99528c810a4a91c63264348c96315c0029
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-09-30 20:01:43 -0300

    refac: new logic to load api and show components

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,36 +1,70 @@
 import { useEffect, useState } from 'react';
 
 import ICard from '../../interfaces/ICard';
-// import Loading from '../Loading';
+import Loading from '../Loading';
 import StyledCard from './style';
 import getDataApi from '../../utils/getApiData';
+import ErrorIcon from '@mui/icons-material/Error';
+import Button from '@mui/material/Button';
 
 function Card(props: ICard) {
-  const [cardContent, setCardContent] = useState(-1);
-  const [selectItem, setSelectItem] = useState(props.options[0].name)
+  const [apiData, setApiData] = useState<number | string>();
+  const [selectItem, setSelectItem] = useState<string>(props.options[0].name);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [mainContent, setMainContent] = useState<JSX.Element | undefined>();
 
   useEffect(() => {
     const dataCard = async () => {
-      const result: number = await getDataApi(`api/covidstatesbr/${selectItem}`);
-      setCardContent(result);
+      const result: number | string = await getDataApi(`api/covidstatesbr/${selectItem}`);
+      if (result === 'err') {
+        setMainContent(reloadData);
+        setIsLoading(false);
+      } else {
+        setApiData(result);
+        setIsLoading(false);
+      }
     }
 
     dataCard();
   }, [selectItem]);
 
-  function changeSelectTextCard(event: React.ChangeEvent<HTMLSelectElement>) {
+  useEffect(() => {
+    setMainContent(sucessfulData);
+  }, [apiData]);
+
+  function handleMainTextCard(event: React.ChangeEvent<HTMLSelectElement>) {
     setSelectItem(event.target.value)
+    setIsLoading(true);
   }
+
+  const sucessfulData = (
+  <p
+    className='primary-info'>{apiData}
+  </p>)
+
+  const reloadData = (
+    <div className='info-to-reload'>
+      <ErrorIcon />
+      <p>Request failed!</p>
+      <Button
+        variant="contained"
+        // onClick={...}
+        className='button-reload'
+      >
+        Reload
+      </Button>
+    </div>
+  )
 
   return (
     <StyledCard>
-      <h2><i>{props.icon}</i>{props.textTop}</h2>
-      <select onChange={ changeSelectTextCard }>
+      <h2><p>{props.icon}</p>{props.textTop}</h2>
+      <select onChange={ handleMainTextCard }>
         { props.options.map(({ id, name }) => (
-          <option key={id} value={name}>{selectItem}</option>
+          <option key={id} value={name}>{name}</option>
           )) }
       </select>
-      <p className='primary-info'>{cardContent}</p>
+      { isLoading ? <Loading /> : mainContent }
       <a className='link-info' href="#">{props.textBottom}</a>
     </StyledCard>
   )
