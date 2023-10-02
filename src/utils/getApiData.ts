8860a7a0e4e4d9c5b0a863558c9266222b12dc46
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-10-02 10:09:56 -0300

    fix: move the try catch to component rendered

--- src/utils/getApiData.ts
+++ src/utils/getApiData.ts
@@ -1,12 +1,8 @@
 import axios from "axios";
 
 async function getDataApi(endpoint: string) {
-  try {
     const { data } = await axios.get(endpoint, { signal: AbortSignal.timeout(4000) });
     return data;
-  } catch (err) {
-    return 'err';
-  }
 }
 
 export default getDataApi;
