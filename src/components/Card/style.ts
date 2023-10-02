c53296640122580b78b85dadebc447a2d8c57e75
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-10-02 10:10:21 -0300

    style: add button reload and more responsivity

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -8,14 +8,16 @@ const StyledCard = styled.section`
   background-color: #f9f9f9;
   width: 380px;
   height: 28vh;
-  padding: 1.5rem;
+  padding: 1rem;
 
 
   & .primary-info {
+    width: 100%;
+    height: 100%;
     grid-row: 2;
     grid-column: 1 / -1;
     align-self: center;
-    font-size: 3rem;
+    font-size: 1.5vw;
   }
 
   & .link-info {
@@ -28,8 +30,8 @@ const StyledCard = styled.section`
     text-align: center;
     grid-column: 1 / 3;
     display: flex;
-    gap: 18px;
-    font-size: 1rem;
+    gap: 10px;
+    font-size: 1.2rem;
     font-weight: 300;
     align-items: center;
   }
@@ -49,6 +51,42 @@ const StyledCard = styled.section`
     justify-self: center;
     align-self: center;
     grid-column: 1 / -1;
+  }
+
+  & .info-to-reload {
+    display: flex;
+    background-color: #ffaeae;
+    border-radius: 5px;
+    grid-column: 1 / -1;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    font-size: 1vw;
+    font-weight: 700;
+    gap: 20px;
+  }
+
+  & .icon-warning {
+    color: red;
+    width: 60px;
+    height: 60px;
+  }
+
+  & .button-reload {
+    background-color: #fff;
+    color: black;
+    font-weight: 600;
+    align-self: center;
+    justify-self: right;
+    text-transform: capitalize;
+  }
+
+  & .button-reload:hover {
+    background-color: #e3e3e3;
+  }
+
+  & .button-reload:active {
+    background-color: #fff;
   }
 `;
 
