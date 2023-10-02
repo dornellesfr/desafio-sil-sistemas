e4fcfce0bc8973a1cdb1dfe3c2a056312caac3eb
fernando dornelles rocha <fe.dornellesrocha@outlook.com>
2023-10-02 10:09:39 -0300

    feat: add new route

--- src/miragejs/index.ts
+++ src/miragejs/index.ts
@@ -1,14 +1,24 @@
 import { createServer } from 'miragejs';
 import brStates from '../helpers/brazilianStates';
 import statesCases from '../helpers/covidCases';
+import jokeCategory from '../helpers/jokeCategory';
+import jokes from '../helpers/chuckJokes';
 
 createServer({
   routes() {
     this.namespace = 'api';
 
-    brStates.forEach(({ name }) => {
+    brStates.forEach((name) => {
       this.get(`covidstatesbr/${name}`, () => {
         return statesCases[name as keyof typeof statesCases];
+      },
+      { timing: 3000 }
+      );
+    });
+
+    jokeCategory.forEach((name) => {
+      this.get(`chucknorrisjoke/${name}`, () => {
+        return jokes[name as keyof typeof jokes];
       },
       { timing: 3000 }
       );
